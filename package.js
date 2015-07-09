afd3e892556470d2246b0a300460b33d81a28763
Ed Kim <edk217@gmail.com>
2015-07-09 16:44:00 -0700

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "edkim:supercalendar",
   summary: "Google Calendar-like as smart package",
-  version: "0.7.1",
+  version: "0.7.2",
   git: "https://github.com/edkim/meteor_supercalendar"
 });
 
